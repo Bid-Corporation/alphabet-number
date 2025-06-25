16d11f595db7569fbee1e7d31aa4ed208ba2f6cc
Jm Macalanda <macalandajohnmichael@gmail.com>
2025-06-25 23:36:44 +0800

    Add pernumbers.php

---
+++ sharedfolder/pernumbers.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Finals</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="../style-perletters.css">
+</head>
+
+
+<body>
+    <div class="video-background">
+        <video autoplay loop muted playsinline src="../background/bgvideo.mp4"></video>
+    </div>
+
+    <div class="audio-background">
+        <audio id="bgAudio" loop src="../background/bgaudio.mp3"></audio>
+    </div>
+
+    <div class="main-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card-letters">
+                        <div class="card-red">
+                            <div class="card-right"></div>
+                        </div>
+
+                        <div class="card-buttons">
+    <div class="card-back">
+        <a href="alphabets.php" class="btn btn-back">
+            <i class="fas fa-arrow-left"></i>
+        </a>
+    </div>
+    <div class="card-home">
+        <a href="alphanum.php" class="btn btn-home">
+            <i class="fas fa-house"></i>
+        </a>
+    </div>
+</div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
