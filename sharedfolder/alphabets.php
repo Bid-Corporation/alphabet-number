3e32b387ca124069362ce0ccd2b40d0e09273a55
Jm Macalanda <macalandajohnmichael@gmail.com>
2025-06-26 14:09:45 +0800

    resizing alpha and number button

--- sharedfolder/alphabets.php
+++ sharedfolder/alphabets.php
@@ -2,106 +2,70 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Alphabets</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    <link rel="stylesheet" href="../style-alphabets.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Alphabets</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+  <link rel="stylesheet" href="../style-alphabets.css">
 </head>
 
 <body>
+  <div class="video-background">
+    <video autoplay loop muted playsinline src="../background/bgvideo.mp4"></video>
+  </div>
 
-    <div class="video-background">
-        <video autoplay loop muted playsinline src="../background/bgvideo.mp4"></video>
-    </div>
+  <div class="audio-background">
+    <audio id="bgAudio" loop src="../background/bgaudio.mp3"></audio>
+  </div>
 
-    <div class="audio-background">
-        <audio id="bgAudio" loop src="../background/bgaudio.mp3"></audio>
-    </div>
+  <div class="container-fluid alphabet-home">
+    <div class="container text-center">
+      <div class="row my-3">
+        <div class="col-12 col-sm-6 mx-auto">
+          <div class="alphabet-title py-3 text-center">Alphabets</div>
+        </div>
+      </div>
 
-    <div class="container-fluid alphabet-home">
-        <div class="container text-center">
-            <div class="row my-3">
-                <div class="col-12 col-sm-6 mx-auto">
-                    <div class="alphabet-title py-3 text-center">
-                        Alphabets
-                    </div>
-                </div>
-            </div>
-            <div class="row my-3">
-                <div class="button-container rounded-5">
-                    <div class="row my-3" id="alphabet-buttons">
-                    </div>
-                </div>
-            </div>
-            <div class="row justify-content-center my-4">
-    <div class="col-auto">
-        <div class="card-back">
+      <div class="button-container" id="alphabet-buttons"></div>
+
+      <div class="row justify-content-center my-4">
+        <div class="col-auto">
+          <div class="card-back">
             <div class="back-button">
-                <a href="alphanum.php">
-                    <i class="fa-solid fa-arrow-left"></i>
-                </a>
+              <a href="alphanum.php">
+                <i class="fa-solid fa-arrow-left"></i>
+              </a>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 
+  <script>
+    const letters = [
+      "Aa", "Bb", "Cc", "Dd", "Ee", "Ff", "Gg", "Hh", "Ii", "Jj",
+      "Kk", "Ll", "Mm", "Nn", "Oo", "Pp", "Qq", "Rr", "Ss", "Tt",
+      "Uu", "Vv", "Ww", "Xx", "Yy", "Zz"
+    ];
 
-                </div>
-            </div>
-        </div>
-    </div>
+    const links = letters.map(letter => "perletters.php?char=" + letter.charAt(0).toUpperCase());
+    const buttonContainer = document.getElementById("alphabet-buttons");
+    let htmlContent = "";
 
-    <script>
-        function toggleMusic(button) {
-            const audio = document.getElementById('bgAudio');
-            const icon = button.querySelector('i');
+    for (let i = 0; i < letters.length; i++) {
+      htmlContent += `
+        <a href="${links[i]}" target="_self">
+          <button class="alphabet-button rounded-5">${letters[i]}</button>
+        </a>
+      `;
+    }
 
-            if (audio.paused) {
-                audio.play();
-                icon.classList.remove('fa-volume-xmark');
-                icon.classList.add('fa-volume-high');
-                button.style.backgroundColor = '#2297EB';
-            } else {
-                audio.pause();
-                icon.classList.remove('fa-volume-high');
-                icon.classList.add('fa-volume-xmark');
-                button.style.backgroundColor = '#D80303';
-            }
-        }
+    buttonContainer.innerHTML = htmlContent;
+  </script>
 
-        const letters = [
-            "Aa", "Bb", "Cc", "Dd", "Ee", "Ff", "Gg", "Hh", "Ii", "Jj",
-            "Kk", "Ll", "Mm", "Nn", "Oo", "Pp", "Qq", "Rr", "Ss", "Tt",
-            "Uu", "Vv", "Ww", "Xx", "Yy", "Zz"
-        ];
-
-        const links = letters.map(letter => "perletters.php?char=" + letter.charAt(0).toUpperCase());
-
-
-
-        var buttonContainer = document.getElementById("alphabet-buttons");
-        
-        var htmlContent = "";
-
-        for (var i = 0; i < letters.length; i++) {
-            htmlContent +=
-                '<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 my-3 text-center">' +
-                    '<a href="' + links[i] + '" target="_self">' +
-                        '<button class="alphabet-button py-3 px-4 rounded-5">' + letters[i] + '</button>' +
-                    '</a>' +
-                '</div>';
-        }
-
-        buttonContainer.innerHTML = htmlContent;
-
-    </script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
-        crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- sharedfolder/numbers.php
+++ sharedfolder/numbers.php
@@ -2,81 +2,58 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Numbers</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    <link rel="stylesheet" href="../style-numbers.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Numbers</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+  <link rel="stylesheet" href="../style-numbers.css">
 </head>
 
 <body>
+  
+  <div class="video-background">
+    <video autoplay loop muted playsinline src="../background/bgvideo.mp4"></video>
+  </div>
 
-    <div class="video-background">
-        <video autoplay loop muted playsinline src="../background/bgvideo.mp4"></video>
-    </div>
+  
+  <div class="audio-background">
+    <audio id="bgAudio" loop src="../background/bgaudio.mp3"></audio>
+  </div>
 
-    <div class="audio-background">
-        <audio id="bgAudio" loop src="../background/bgaudio.mp3"></audio>
-    </div>
+  <div class="container-fluid number-home">
+    <div class="container text-center">
 
-    <div class="container-fluid number-home">
-        <div class="container text-center">
-            <!-- Title -->
-            <div class="row my-3">
-                <div class="col-12 col-sm-6 mx-auto">
-                    <div class="alphabet-title py-3 text-center">
-                        Numbers
-                    </div>
-                </div>
-            </div>
-
-            <!-- Buttons -->
-            <div class="row my-3 justify-content-center">
-                <div class="col-12">
-                    <div class="button-container rounded-5">
-                        <div class="row justify-content-center my-3" id="number-buttons">
-                            <!-- Number buttons will be inserted here -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Back button -->
-            <div class="row justify-content-center my-4">
-                <div class="col-auto">
-                    <div class="card-back">
-                        <a href="alphanum.php" class="back-button">
-                            <i class="fa-solid fa-arrow-left"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
+      <div class="row my-3">
+        <div class="col-12 col-sm-6 mx-auto">
+          <div class="alphabet-title py-3 text-center">Numbers</div>
         </div>
+      </div>
+
+      <div class="numbers-container mx-auto my-3">
+        <?php
+          $numbers = range(0, 9);
+          foreach ($numbers as $num) {
+              echo '<a href="pernumbers.php?num=' . $num . '" target="_self">
+                      <button class="numbers-button rounded-5">' . $num . '</button>
+                    </a>';
+          }
+        ?>
+      </div>
+      <div class="row justify-content-center my-4">
+        <div class="col-auto">
+          <div class="card-back">
+            <a href="alphanum.php" class="back-button">
+              <i class="fa-solid fa-arrow-left"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+
     </div>
+  </div>
 
-    <script>
-        const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" , "10"];
-        const links = numbers.map(num => "pernumbers.php?num=" + num);
-        const buttonContainer = document.getElementById("number-buttons");
-
-        let htmlContent = "";
-        for (let i = 0; i < numbers.length; i++) {
-            htmlContent += `
-                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 my-3 text-center">
-                    <a href="${links[i]}" target="_self">
-                        <button class="numbers-button py-3 px-4 rounded-5">${numbers[i]}</button>
-                    </a>
-                </div>`;
-        }
-        buttonContainer.innerHTML = htmlContent;
-    </script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
-        crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
