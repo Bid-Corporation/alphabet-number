bdd08614e2b00db3625d40a31e5456fbca3e1b5b
Carl Joseph Poonin <cjaypoonin@gmail.com>
2025-06-27 10:56:37 +0800

    remove line space

--- admin.php
+++ admin.php
@@ -17,11 +17,10 @@ $numRes   = mysqli_query($conn, $numSql);
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Admin • Database Tables</title>
-
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
   <link rel="stylesheet" href="style.css">
-
+  
   <style>
     .table-wrapper {
       backdrop-filter: blur(6px);
